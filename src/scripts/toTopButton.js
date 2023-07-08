8f6e2e78b75d05b124bef236b5223678f31a515f
Rutger <1201954@student.roc-nijmegen.nl>
2023-07-09 00:19:27 +0200

    back to top button

---
+++ src/scripts/toTopButton.js
@@ -0,0 +1,9 @@
+const toTopButton = document.querySelector('#to-top-button');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        toTopButton.classList.add('opacity-100');
+    } else {
+        toTopButton.classList.remove('opacity-100');
+    }
+});

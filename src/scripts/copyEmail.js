a8b37e9e9bb65dc204540f8dd3904b4695baf36e
Rutger <1201954@student.roc-nijmegen.nl>
2023-07-13 16:20:39 +0200

    added a hover effect to email.

--- src/scripts/copyEmail.js
+++ src/scripts/copyEmail.js
@@ -4,13 +4,17 @@ const copyToClipboard = (Event) => {
 
     navigator.clipboard.writeText(email);
 
+    emailHover.classList.remove('group-hover:block');
     emailMessage.classList.remove('hidden');
 
     setTimeout(() => {
         emailMessage.classList.add('hidden');
+        emailHover.classList.add('group-hover:block');
     }, 1500);
 };
 
 const emailButton = document.querySelector('#copy-email');
 const emailMessage = document.querySelector('#copy-mail-succes');
+const emailHover = document.querySelector('#copy-mail-hover');
+
 emailButton.addEventListener('click', copyToClipboard);

f471b8e845ccaa71c19358a3bb1d958c5685cacb
Rutger <1201954@student.roc-nijmegen.nl>
2023-07-15 15:36:03 +0200

    added animate on scroll

---
+++ src/scripts/animateOnScroll.js
@@ -0,0 +1,12 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate');
+        }
+    });
+});
+
+const elements = document.querySelectorAll('.animate-on-scroll');
+
+elements.forEach((element) => observer.observe(element));

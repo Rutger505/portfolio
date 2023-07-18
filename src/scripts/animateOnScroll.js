bc458a29e2636a57b013872989aa4c5eb019aad7
Rutger <1201954@student.roc-nijmegen.nl>
2023-07-19 00:38:14 +0200

    Start changing animations

--- src/scripts/animateOnScroll.js
+++ src/scripts/animateOnScroll.js
@@ -1,11 +1,16 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('animate');
-    }
-  });
-});
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
 
-const elements = document.querySelectorAll('.animate-on-scroll');
+const elements = document.querySelectorAll('.animate-on-scroll, .animate-top, .animate-bottom, .animate-left, .animate-right');
 
 elements.forEach((element) => observer.observe(element));

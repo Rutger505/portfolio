d4005fbbdeccf56541aed048044ab8484b7247c2
Rutger <rutger.pronk11@gmail.com>
2023-10-13 16:05:47 +0200

    Complete translation

--- src/index.php
+++ src/index.php
@@ -37,7 +37,7 @@ require_once "language.php";
           <li class="hidden md:block">
             <a class="group p-2" href="#about-me">
               <div class="overflow-hidden text-header duration-300 group-hover:opacity-80">
-                <p class="about-me"><?php echo text('aboutMe') ?></p>
+                <p class="about-me"><?php echo text('aboutMeHeader') ?></p>
                 <span class="header-underline block h-[3px] w-full translate-x-[-101%] rounded-full bg-accent duration-300 group-hover:translate-x-[-50%]"></span>
               </div>
             </a>
@@ -45,7 +45,7 @@ require_once "language.php";
           <li class="hidden md:block">
             <a class="group p-2" href="#my-work">
               <div class="overflow-hidden text-header duration-300 group-hover:opacity-80">
-                <p class="my-work"><?php echo text('myWork') ?></p>
+                <p class="my-work"><?php echo text('myWorkHeader') ?></p>
                 <span class="header-underline block h-[3px] w-full translate-x-[-101%] rounded-full bg-accent duration-300 group-hover:translate-x-[-50%]"></span>
               </div>
             </a>
@@ -53,7 +53,7 @@ require_once "language.php";
           <li class="hidden md:block">
             <a class="group p-2" href="#contact">
               <div class="overflow-hidden text-header duration-300 group-hover:opacity-80">
-                <p class="contact"><?php echo text('contact') ?></p>
+                <p class="contact"><?php echo text('contactHeader') ?></p>
                 <span class="header-underline block h-[3px] w-full translate-x-[-101%] rounded-full bg-accent duration-300 group-hover:translate-x-[-50%]"></span>
               </div>
             </a>
@@ -95,17 +95,17 @@ require_once "language.php";
       <ul class="flex flex-col items-center">
         <li>
           <a class="group block h-fit p-4" href="#about-me">
-            <p class="about-me overflow-hidden text-header duration-300 group-focus:text-accent"><?php echo text('aboutMe') ?></p>
+            <p class="about-me overflow-hidden text-header duration-300 group-focus:text-accent"><?php echo text('aboutMeHeader') ?></p>
           </a>
         </li>
         <li>
           <a class="group block h-fit p-4" href="#my-work">
-            <p class="my-work overflow-hidden text-header duration-300 group-focus:text-accent"><?php echo text('myWork') ?></p>
+            <p class="my-work overflow-hidden text-header duration-300 group-focus:text-accent"><?php echo text('myWorkHeader') ?></p>
           </a>
         </li>
         <li>
           <a class="group block h-fit p-4" href="#contact">
-            <p class="contact overflow-hidden text-header duration-300 group-focus:text-accent"><?php echo text('contact') ?></p>
+            <p class="contact overflow-hidden text-header duration-300 group-focus:text-accent"><?php echo text('contactHeader') ?></p>
           </a>
         </li>
         <?php if ($language == "nl") : ?>
@@ -135,14 +135,14 @@ require_once "language.php";
   <main id="home" class="mx-spacing flex min-h-screen flex-col">
     <h2 class="animate-y-md mt-60 text-mobileAroundXl text-text delay-75 xs:text-aroundXl lg:mt-80">&lt;/&gt; Software-Dev</h2>
     <h1 id="home-text" class="animate-y text-mobileXl leading-tight xs:text-xl max-w-[700px]">
-      Hallo, ik ben Rutger uit Nederland
+      <?php echo text('landingPageText') ?>
     </h1>
     <script src="scripts/highlightRutger.js"></script>
 
 
     <div class="animate-y-lg mt-36 delay-300 lg:mt-40 xl:mt-40">
       <a class="w-fit" href="#my-work">
-        <p id="explore-projects" class="inline-block text-link text-text">Ontdek mijn projecten</p>
+        <p class="inline-block text-link text-text"><?php echo text('exploreProjects') ?></p>
         <img class="ml-1 inline-block" src="img/arrow-down-icon.svg" alt="arrow down" />
       </a>
     </div>
@@ -159,7 +159,7 @@ require_once "language.php";
   </main>
 
   <section id="about-me" class="mx-spacing py-24 mdlg:py-32">
-    <h1>Wie ben ik?</h1>
+    <h1><?php echo text('aboutMeTitle') ?></h1>
     <div class="mt-10 flex flex-wrap gap-x-12 gap-y-10 mdlg:flex-nowrap lg:gap-x-24">
       <div class="flex flex-wrap items-center justify-center gap-10 md:flex-shrink-0 mdlg:flex-col mdlg:flex-nowrap">
         <img class="w-full max-w-[20rem] rounded-full bg-secondary sm:h-80 sm:w-80" src="img/personal-photo.webp" alt="personal-photo" />
@@ -178,36 +178,29 @@ require_once "language.php";
       </div>
       <div class="mt-8 max-w-[572px] flex-shrink delay-150 sm:min-w-[350px]">
         <p class="animate-y-sm">
-          Mijn naam is Rutger Pronk en ik studeer momenteel om een software developer te worden. Ik zit momenteel in mijn eerste studiejaar op het ROC Nijmegen.
+          <?php echo text('aboutMeText1') ?>
         </p>
-        <p class="animate-y-sm mt-7 delay-75">
-          Ik heb een grote passie voor programmeren en softwareontwikkeling, met een specifieke focus op het uitbrijden van mijn kennis in Java en C#. Daarnaast leer ik
-          ook zowel front-end als back-end voor websites.
+        <p class="animate-y-sm mt-7 delay-75"><?php echo text('aboutMeText2') ?>
         </p>
       </div>
     </div>
   </section>
 
   <section id="my-work" class="mx-spacing py-24 mdlg:py-32">
-    <h1>Wat kan ik doen?</h1>
+    <h1> <?php echo text('myWorkTitle') ?> </h1>
 
-    <p class="animate-y-sm mt-10 max-w-[710px]">
-      Ik vind het leuk om te werken met Java, C# en websites (HTML, CSS, JavaScript, PHP, SQL). Ik heb samengewerkt met projecten, waarbij ik mijn teamwerk en
-      communicatie skills heb verbeterd. Ik maak gebruik van Git/GitHub voor versiebeheer en Scrum/Trello voor projectmanagement. Hier zijn enkele projecten die mijn
-      vaardigheden laten zien.
+    <p class="animate-y-sm mt-10 max-w-[710px]"><?php echo text('myWorkText') ?>
     </p>
-    <h2 id="my-work-projects-title" class="animate-y-md mt-10">Mijn werk</h2>
+    <h2 class="animate-y-md mt-10">Mijn werk <?php echo text('myWorkSubtitle') ?></h2>
     <div class="mt-10 flex flex-col items-start gap-y-8">
       <div class="animate-y-sm flex h-fit flex-wrap justify-between gap-x-7 rounded-lg bg-secondary px-10 py-10 lg:px-20">
         <div class="flex flex-col items-start">
-          <h2 id="autoclicker-title">Useful Autoclicker</h2>
-          <p id="autoclicker-explanation" class="mt-5 max-w-[430px]">
-            Een GUI-project in development. Oorspronkelijk gemaakt in Java, wordt het nu herschreven in C#. Deze autoclicker-tool is een project waar ik de afgelopen
-            zes maanden aan heb gewerkt.
+          <h2><?php echo text('usefulAutoclickerTitle') ?></h2>
+          <p class="mt-5 max-w-[430px]"><?php echo text('usefulAutoclickerText') ?>
           </p>
           <div class="flex-1"></div>
           <a class="button group mt-5" href="https://github.com/Rutger505/Useful-Autoclicker" target="_blank">
-            <p class="go-to-github inline">Ga naar Github repo</p>
+            <p class="go-to-github inline"><?php echo text('goToGithub') ?></p>
             <svg class="inline-block fill-detail group-hover:fill-text" width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M1.64108 20L0 18.3658L16.0982 2.33463H1.32849V0H20.0837V18.677H17.7393V3.96887L1.64108 20Z" fill-opacity="0.702" />
             </svg>
@@ -217,14 +210,13 @@ require_once "language.php";
       </div>
       <div class="animate-y-sm flex h-fit flex-wrap justify-between gap-x-7 rounded-lg bg-secondary px-10 py-10 lg:px-20">
         <div class="flex flex-col items-start">
-          <h2 id="travel-agency-title">Reisbureau</h2>
-          <p id="travel-agency-explanation" class="mt-5 max-w-[430px]">
-            Een project waarbij gebruik wordt gemaakt van HTML, CSS, Tailwind CSS, PHP, MySQL en API-integratie. Ik heb drie maanden samengewerkt met een andere
-            ontwikkelaar om deze reisbureau-website te ontwikkelen.
+          <h2><?php echo text('travelAgencyTitle') ?></h2>
+          <p class="mt-5 max-w-[430px]">
+            <?php echo text('travelAgencyText') ?>
           </p>
           <div class="flex-1"></div>
           <a class="button group mt-5" href="https://github.com/Rutger505/Reisbureau" target="_blank">
-            <p class="go-to-github inline">Ga naar Github repo</p>
+            <p class="go-to-github inline"><?php echo text('goToGithub') ?></p>
             <svg class="inline-block fill-detail group-hover:fill-text" width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M1.64108 20L0 18.3658L16.0982 2.33463H1.32849V0H20.0837V18.677H17.7393V3.96887L1.64108 20Z" fill-opacity="0.702" />
             </svg>
@@ -232,16 +224,14 @@ require_once "language.php";
         </div>
         <img class="mt-7 max-h-[320px]" src="img/reisbureau.webp" alt="useful autoclicker" />
       </div>
-      <div class="animate-y-sm flex h-fit flex-wrap justify-between rounded-lg bg-secondary px-10 py-10 lg:px-20">
+      <div class="animate-y-sm flex h-fit flex-wrap justify-between gap-x-7 rounded-lg bg-secondary px-10 py-10 lg:px-20">
         <div class="flex flex-col items-start">
-          <h2 id="no-time-to-die-title">No Time To Die</h2>
-          <p id="no-time-to-die-explanation" class="mt-5 max-w-[430px]">
-            A gamejam project, this is a round-based survival game created in just 16 hours using Gamemaker. I collaborated with a team of 5 members to deliver this
-            game.
+          <h2><?php echo text('noTimeToDieTitle') ?></h2>
+          <p class="mt-5 max-w-[430px]"><?php echo text('noTimeToDieText') ?>
           </p>
           <div class="flex-1"></div>
           <a class="button mt-5" href="https://github.com/Rutger505/Gamejam" target="_blank">
-            <p class="go-to-github inline">Ga naar Github repo</p>
+            <p class="go-to-github inline"><?php echo text('goToGithub') ?></p>
             <svg class="inline-block fill-detail group-hover:fill-text" width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M1.64108 20L0 18.3658L16.0982 2.33463H1.32849V0H20.0837V18.677H17.7393V3.96887L1.64108 20Z" fill-opacity="0.702" />
             </svg>
@@ -250,31 +240,31 @@ require_once "language.php";
         <img class="mt-7 max-h-[320px]" src="img/no-time-to-die.webp" alt="useful autoclicker" />
       </div>
     </div>
-    <h2 id="my-work-experience-title" class="animate-y-md mt-20">Mijn ervaringen</h2>
+    <h2 class="animate-y-md mt-20"><?php echo text('experiencesTitle') ?></h2>
     <div class="mt-10 flex">
       <div class="w-2 flex-shrink-0 rounded-full bg-secondary"></div>
       <div class="flex flex-col gap-y-14">
         <div class="ml-[-12px] flex items-start gap-x-10">
           <div class="h-4 w-4 flex-shrink-0 rounded-full bg-accent"></div>
           <div class="flex flex-col gap-2">
-            <p id="my-work-experiences-times-future" class="my-work-experiences-times mb-2">Toekomst</p>
+            <p class="my-work-experiences-times mb-2"><?php echo text('experiencesFuture') ?> </p>
             <div>
-              <h2 id="my-work-experiences-explanation-continue" class="animate-yleading-tight">Door studeren</h2>
-              <p id="my-work-experiences-location-continue" class="animate-y delay-75">Andere School - HBO</p>
+              <h2 class="animate-y leading-tight"><?php echo text('experiencesFutureStudy') ?></h2>
+              <p class="animate-y delay-75"><?php echo text('experiencesFutureStudyLocation') ?></p>
             </div>
           </div>
         </div>
         <div class="ml-[-12px] flex items-start gap-x-10">
           <div class="h-4 w-4 flex-shrink-0 rounded-full bg-accent"></div>
           <div class="flex flex-col gap-2">
-            <p id="my-work-experiences-times-2022" class="mb-2 text-accent">2022 - Heden</p>
+            <p class="mb-2 text-accent"><?php echo text('experiencesPresent') ?></p>
             <div>
-              <h2 id="my-work-experiences-explanation-student" class="animate-y leading-tight">Student Software Developer</h2>
-              <p id="my-work-experiences-location-student" class="animate-y my-work-experiences-location delay-75">ROC Nijmegen Technovium - MBO</p>
+              <h2 class="animate-y leading-tight"><?php echo text('experiencesROCSoftwareDeveloper') ?></h2>
+              <p class="animate-y my-work-experiences-location delay-75"><?php echo text('experiencesROCSoftwareDeveloperLocation') ?></p>
             </div>
             <div>
-              <h2 id="my-work-experiences-explanation-intern-gemini" class="animate-y my-work-experiences-explanation leading-tight delay-75">Stage embedded leren</h2>
-              <p id="my-work-experiences-location-intern-gemini" class="animate-y my-work-experiences-location delay-150">Gemini Embedded Technologies</p>
+              <h2 class="animate-y my-work-experiences-explanation leading-tight delay-75"><?php echo text('experiencesGemini') ?></h2>
+              <p class="animate-y my-work-experiences-location delay-150"><?php echo text('experiencesGeminiLocation') ?></p>
             </div>
           </div>
         </div>
@@ -283,16 +273,16 @@ require_once "language.php";
   </section>
 
   <section id="contact" class="mx-spacing py-24 mdlg:py-32">
-    <h1>Neem contact op</h1>
+    <h1><?php echo text('contactTitle') ?></h1>
     <div class="flex flex-wrap items-start justify-between gap-7 lg:flex-nowrap">
       <div class="flex flex-wrap items-start justify-start gap-0">
         <div class="animate-y max-w-[750px]">
-          <p id="contact-text" class="inline">Voel je vrij om contact met me op te nemen via het formulier hier of rechtstreeks via e-mail op</p>
+          <p class="inline"><?php echo text('contactText') ?></p>
           <span class="inline-flex">
             <button id="copy-email" class="group relative inline cursor-pointer text-detail underline">
               <p id="copy-email-address">rutger.pronk19@gmail.com</p>
               <div id="copy-mail-succes" class="absolute -top-14 left-7 m-auto -translate-y-5 rounded bg-secondary p-2 text-text opacity-0 transition-all">
-                Email gekopieërd!
+                <?php echo text('contactTextEmailCopy') ?>
               </div>
             </button>
             <p>.</p>
@@ -301,13 +291,13 @@ require_once "language.php";
       </div>
 
       <div class="animate-y-sm flex min-w-[80%] flex-col rounded-lg bg-secondary p-7 delay-300 sm:min-w-[370px]">
-        <h2 id="contact-form-title">Stuur me een bericht</h2>
-        <input id="contact-form-name" class="mt-6 w-full border-b-2 border-detail bg-transparent px-1 pb-2 text-text outline-none" type="text" placeholder="Naam" />
-        <input id="contact-form-email" class="mt-6 w-full border-b-2 border-detail bg-transparent px-1 pb-2 text-text outline-none" type="text" placeholder="Email" />
-        <input id="contact-form-subject" class="mt-6 w-full border-b-2 border-detail bg-transparent px-1 pb-2 text-text outline-none" type="text" placeholder="Onderwerp" />
-        <textarea id="contact-form-message" class="mt-6 min-h-[120px] w-full border-b-2 border-detail bg-transparent px-1 pb-2 text-text outline-none" type="text" placeholder="Bericht"></textarea>
-        <button id="contact-form-button" class="button mt-10">Verstuur bericht</button>
-        <p id="contact-form-warning" class="mt-3 text-red">In contructie</p>
+        <h2><?php echo text('contactFormTitle') ?></h2>
+        <input class="mt-6 w-full border-b-2 border-detail bg-transparent px-1 pb-2 text-text outline-none" type="text" placeholder="<?php echo text('contactFormName') ?>" />
+        <input class="mt-6 w-full border-b-2 border-detail bg-transparent px-1 pb-2 text-text outline-none" type="text" placeholder="<?php echo text('contactFormEmail') ?>" />
+        <input class="mt-6 w-full border-b-2 border-detail bg-transparent px-1 pb-2 text-text outline-none" type="text" placeholder="<?php echo text('contactFormSubject') ?>" />
+        <textarea class="mt-6 min-h-[120px] w-full border-b-2 border-detail bg-transparent px-1 pb-2 text-text outline-none" type="text" placeholder="<?php echo text('contactFormMessage') ?>"></textarea>
+        <button class="button mt-10"><?php echo text('contactFormSubmit') ?></button>
+        <p class="mt-3 text-red"><?php echo text('contactFormConstruction') ?></p>
       </div>
     </div>
   </section>
@@ -326,6 +316,7 @@ require_once "language.php";
     </div>
     <p>© 2023 Rutger Pronk</p>
   </footer>
+
   <script src="scripts/smoothNavigation.js"></script>
   <script src="scripts/compactHeader.js"></script>
   <script src="scripts/underlineHeader.js"></script>
@@ -334,7 +325,6 @@ require_once "language.php";
   <script src="scripts/animateOnScroll.js"></script>
   <script src="scripts/dropdownHeader.js"></script>
   <script src="scripts/backgroundBlur.js"></script>
-  <script src="scripts/languageHandeler.js"></script>
 </body>
 
 </html>

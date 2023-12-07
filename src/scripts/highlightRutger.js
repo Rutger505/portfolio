3509973b2818f9f05314c6b5640636a1a4440a36
Rutger <rutger.pronk11@gmail.com>
2023-12-07 21:09:23 +0100

    Initial commit

--- src/scripts/highlightRutger.js
+++
@@ -1,6 +0,0 @@
-const textElement = document.querySelector('#home-text');
-
-const originalText = textElement.innerHTML;
-const newText = originalText.replace('Rutger', '<span class="text-accent">Rutger</span>');
-
-textElement.innerHTML = newText;

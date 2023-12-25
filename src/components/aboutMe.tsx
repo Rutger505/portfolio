afb08188dc9295a500c8a67896ee38e7721fef7e
Rutger <rutger.pronk11@gmail.com>
2023-12-25 12:28:35 +0100

    Update text, remove links from about me.

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -1,18 +1,15 @@
 import mePhoto from "../../public/me.webp";
 import Image from "next/image";
-import GithubIcon from "@/components/icons/githubIcon";
-import LinkedinIcon from "@/components/icons/linkedinIcon";
-import MailIcon from "@/components/icons/mailIcon";
 
 export default function AboutMe() {
   return (
     <section
       id="about-me"
-      className="mx-spacing mdlg:py-32 too-big:m-auto max-w-[2500px] py-24"
+      className="mx-spacing max-w-[2500px] py-24 mdlg:py-32 too-big:m-auto"
     >
-      <h2 className="text-xl font-bold">Wie ben ik?</h2>
-      <div className="mdlg:flex-nowrap mt-24 flex flex-wrap gap-x-12 gap-y-10 lg:gap-x-24">
-        <div className="mdlg:flex-col mdlg:flex-nowrap flex flex-wrap items-center justify-center gap-10 md:flex-shrink-0">
+      <h2 className="text-5xl font-bold text-textPrimary">Wie ben ik?</h2>
+      <div className="mt-24 flex flex-wrap items-center gap-x-12 gap-y-10 mdlg:flex-nowrap lg:gap-x-24">
+        <div className="flex flex-wrap items-center justify-center gap-10 md:flex-shrink-0 mdlg:flex-col mdlg:flex-nowrap">
           <Image
             className={
               "w-full max-w-[20rem] rounded-full bg-secondary sm:h-80 sm:w-80"
@@ -20,33 +17,16 @@ export default function AboutMe() {
             src={mePhoto}
             alt={"A picture of me"}
           />
-
-          <div className="aboutMe:flex-col mdlg:flex-row flex flex-row flex-wrap items-center gap-9">
-            <a href="https://github.com/Rutger505" target="_blank">
-              <GithubIcon className={"h-8 text-accent"} />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/rutger-pronk-585149273/"
-              target="_blank"
-            >
-              <LinkedinIcon className={"h-8 text-accent"} />
-            </a>
-            <a href="#contact">
-              <MailIcon className={"h-8 text-accent"} />
-            </a>
-          </div>
         </div>
-        <div className="mt-8 max-w-[572px] flex-shrink delay-150 sm:min-w-[350px]">
+        <div className="mt-8 max-w-[572px] flex-shrink text-textSecondary delay-150 sm:min-w-[350px]">
           <p>
-            Mijn naam is Rutger Pronk en ik studeer momenteel om een software
-            developer te worden. Ik zit momenteel in mijn eerste studiejaar op
-            het ROC Nijmegen.
+            Mijn naam is Rutger Pronk en ik studeer Software Development. Ik zit
+            momenteel in mijn tweede studiejaar op het ROC Nijmegen.
           </p>
           <p className="mt-7">
             Ik heb een grote passie voor programmeren en softwareontwikkeling,
-            met een specifieke focus op het uitbreiden van mijn kennis in Java
-            en C#. Daarnaast leer ik ook zowel front-end als back-end voor
-            websites.
+            met een specifieke focus op het uitbreiden van mijn kennis C# en
+            Next.js. Daarnaast leer ik ook Nest.js voor de backend.
           </p>
         </div>
       </div>

4b57fa1f819c943230339a0386338a0db485d905
Rutger <rutger.pronk11@gmail.com>
2023-12-25 16:02:35 +0100

    Complete landingPage.tsx

--- src/components/landingPage.tsx
+++ src/components/landingPage.tsx
@@ -5,19 +5,25 @@ export default function LandingPage() {
   return (
     <section
       id={"home"}
-      className={"flex min-h-screen flex-col items-center justify-center"}
+      className={"flex min-h-screen flex-col items-center justify-between"}
     >
-      <h2 className={"text-4xl text-textPrimary"}>&lt;/&gt; Software-dev</h2>
-      <h1
-        className={
-          "mb-20 max-w-2xl text-center text-6xl font-bold text-textPrimary"
-        }
-      >
-        Hi, I'm <span className={"text-accent"}>Rutger</span> from the
-        Netherlands
-      </h1>
+      <div></div>
 
-      <Link href={"#about-me"} className={"justify-self-end"}>
+      <div>
+        <h2 className={"text-center text-4xl text-textPrimary"}>
+          &lt;/&gt; Software-dev
+        </h2>
+        <h1
+          className={
+            "mb-10 max-w-2xl text-center text-6xl font-bold text-textPrimary"
+          }
+        >
+          Hi, I&apos;m <span className={"text-accent"}>Rutger</span> from the
+          Netherlands
+        </h1>
+      </div>
+
+      <Link href={"#about-me"} className={"mb-6"}>
         <ArrowDownIcon className={"h-8 animate-bounce text-accent"} />
       </Link>
     </section>

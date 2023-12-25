dc57b8c54b71c555fd3a5d899a87975ad6865e7b
Rutger <rutger.pronk11@gmail.com>
2023-12-25 16:10:42 +0100

    Translate About me Text from dutch to english

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -11,22 +11,21 @@ export default function AboutMe() {
       <div className="mt-24 flex flex-wrap items-center gap-x-12 gap-y-10 mdlg:flex-nowrap lg:gap-x-24">
         <div className="flex flex-wrap items-center justify-center gap-10 md:flex-shrink-0 mdlg:flex-col mdlg:flex-nowrap">
           <Image
-            className={
-              "w-full max-w-[20rem] rounded-full bg-secondary sm:h-80 sm:w-80"
-            }
+            className={"w-full max-w-[20rem] rounded-full sm:h-80 sm:w-80"}
             src={mePhoto}
             alt={"A picture of me"}
           />
         </div>
         <div className="mt-8 max-w-[572px] flex-shrink text-textSecondary delay-150 sm:min-w-[350px]">
           <p>
-            Mijn naam is Rutger Pronk en ik studeer Software Development. Ik zit
-            momenteel in mijn tweede studiejaar op het ROC Nijmegen.
+            My name is Rutger Pronk, and I am studying Software Development at
+            ROC Nijmegen in the Netherlands. I am currently a second year
+            student.
           </p>
           <p className="mt-7">
-            Ik heb een grote passie voor programmeren en softwareontwikkeling,
-            met een specifieke focus op het uitbreiden van mijn kennis C# en
-            Next.js. Daarnaast leer ik ook Nest.js voor de backend.
+            I have a strong passion for programming and software development,
+            with a dedicated focus on improving my skills in C#/C++ and Next.js.
+            Additionally, I am learning Nest.js for back-end web development.
           </p>
         </div>
       </div>

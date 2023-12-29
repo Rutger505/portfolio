4176887ee44483c02db49a9adb7c06ff9fc3d958
Rutger <rutger.pronk11@gmail.com>
2023-12-29 14:39:33 +0100

    add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+import AnimatedButton from "@/components/AnimatedButton";
+import Link from "next/link";
+
+export default function Custom404() {
+  return (
+    <div className={"flex h-screen w-screen items-center justify-center"}>
+      <div className={"flex flex-col items-center"}>
+        <div className={"flex items-center gap-x-4"}>
+          <h1 className={"text-2xl font-medium text-textPrimary"}>404</h1>
+          <div className={"h-10 border-l border-textSecondary opacity-70"} />
+          <p className={"text-textSecondary"}>This page could not be found.</p>
+        </div>
+        <Link href={"/"} className={"mt-10"}>
+          <AnimatedButton text={"Go Home"} />
+        </Link>
+      </div>
+    </div>
+  );
+}

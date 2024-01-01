56efd45e16a76ac3aa0ed5f68ab9d359378910ca
Rutger <rutger.pronk11@gmail.com>
2024-01-01 17:40:28 +0100

    Refactor header to use useState and useEffect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LanguageIcon from "./icons/LanguageIcon";
 import { Link as ScrollLink } from "react-scroll";
 import Link from "next/link";
@@ -8,58 +8,34 @@ import Link from "next/link";
 export default function Header() {
   const scrollAnimation = "easeInOutCubic";
   const scrollDuration = 700;
-  const hideUnderlineClass = "w-0";
-  const showUnderlineClass = "w-full";
-  const hoverUnderlineClass = "group-hover:w-1/2";
+  const [activeSectionIndex, setActiveSectionIndex] = useState<number>(-1);
 
   function changeLanguage() {
     alert("To be implemented"); // TODO Implement language change
   }
 
-  function hideUnderlines(underlines: NodeListOf<Element>) {
-    underlines.forEach((underline) => {
-      underline.classList.add(hideUnderlineClass);
-      underline.classList.add(hoverUnderlineClass);
-      underline.classList.remove(showUnderlineClass);
-    });
-  }
+  function handleLinkUnderline() {
+    const sections = Array.from(document.querySelectorAll("section"));
+    sections.shift(); // remove landing page
+    const centerOfScreen = window.scrollY + window.innerHeight / 2;
 
-  function showUnderline(underline: Element) {
-    underline.classList.add(showUnderlineClass);
-    underline.classList.remove(hoverUnderlineClass);
-    underline.classList.remove(hideUnderlineClass);
-  }
-
-  function getCurrentSectionIndex(
-    sections: HTMLElement[],
-    scrollPosition: number,
-  ): number {
-    const centerOfScreen = scrollPosition + window.innerHeight / 2;
-
-    return sections.findIndex((section) => {
+    const currentSectionIndex = sections.findIndex((section) => {
       const sectionTop = section.offsetTop;
       const sectionBottom = sectionTop + section.offsetHeight;
 
       return centerOfScreen >= sectionTop && centerOfScreen <= sectionBottom;
     });
+
+    setActiveSectionIndex(currentSectionIndex);
   }
 
-  function handleLinkUnderline() {
-    // search sections based on a with href starting with #
-    const allSections = document.querySelectorAll("section");
-    const sections = Array.from(allSections).slice(1); // remove landing page
-    const underlines = document.querySelectorAll(".header-underline");
-    const scrollPosition = window.scrollY;
+  useEffect(() => {
+    window.addEventListener("scroll", handleLinkUnderline);
 
-    const currentSection = getCurrentSectionIndex(sections, scrollPosition);
-
-    hideUnderlines(underlines);
-    if (currentSection !== -1) {
-      showUnderline(underlines[currentSection]);
-    }
-  }
-
-  window.addEventListener("scroll", handleLinkUnderline);
+    return () => {
+      window.removeEventListener("scroll", handleLinkUnderline);
+    };
+  }, []);
 
   return (
     <header className="fixed left-0 top-0 flex h-20 w-screen items-center justify-between bg-secondary px-spacing transition-all duration-500 md:bg-transparent md:backdrop-blur">
@@ -85,9 +61,9 @@ export default function Header() {
         >
           <p>About me</p>
           <span
-            className={
-              "header-underline block h-1 w-0 rounded-full bg-accent duration-300 ease-in-out group-hover:w-1/2"
-            }
+            className={`${
+              activeSectionIndex == 0 ? "w-full" : "w-0 group-hover:w-1/2"
+            } block h-1 rounded-full bg-accent duration-300 ease-in-out `}
           ></span>
         </ScrollLink>
         <ScrollLink
@@ -101,9 +77,9 @@ export default function Header() {
         >
           <p> My work</p>
           <span
-            className={
-              "header-underline block h-1 w-0 rounded-full bg-accent duration-300 ease-in-out group-hover:w-1/2"
-            }
+            className={`${
+              activeSectionIndex == 1 ? "w-full" : "w-0 group-hover:w-1/2"
+            } block h-1 rounded-full bg-accent duration-300 ease-in-out`}
           ></span>
         </ScrollLink>
         <ScrollLink
@@ -117,9 +93,9 @@ export default function Header() {
         >
           <p>Contact</p>
           <span
-            className={
-              "header-underline block h-1 w-0 rounded-full bg-accent duration-300 ease-in-out group-hover:w-1/2"
-            }
+            className={`${
+              activeSectionIndex == 2 ? "w-full" : "w-0 group-hover:w-1/2"
+            } block h-1 rounded-full bg-accent duration-300 ease-in-out`}
           ></span>
         </ScrollLink>
         <Link
@@ -135,7 +111,7 @@ export default function Header() {
           </div>
           <span
             className={
-              "header-underline block h-1 w-0 rounded-full bg-accent duration-300 ease-in-out group-hover:w-1/2"
+              "block h-1 w-0 rounded-full bg-accent duration-300 ease-in-out group-hover:w-1/2"
             }
           ></span>
         </Link>

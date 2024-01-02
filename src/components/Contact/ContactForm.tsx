7fc294cb37a991945a69c77b30fd199c7d30bd43
Rutger <rutger.pronk11@gmail.com>
2024-01-02 20:41:12 +0100

    Make contact form validate input.

--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -1,13 +1,67 @@
 "use client";
 
-import { FormEvent } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import AnimatedButton from "@/components/AnimatedButton";
 
 export default function ContactForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [emailValid, setEmailValid] = useState(true);
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [nameUnfocused, setNameUnfocused] = useState(false);
+  const [emailUnfocused, setEmailUnfocused] = useState(false);
+  const [subjectUnfocused, setSubjectUnfocused] = useState(false);
+  const [messageUnfocused, setMessageUnfocused] = useState(false);
+
+  function validateForm() {
+    return !!name && emailValid && !!subject && !!message;
+  }
+
+  function validateEmail() {
+    const regExp = /\S+@\S+\.\S+/;
+    setEmailValid(regExp.test(email));
+  }
+
+  function onInputBlur(e: FormEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    const inputName = e.currentTarget.name;
+
+    switch (inputName) {
+      case "name":
+        setNameUnfocused(true);
+        break;
+      case "email":
+        setEmailUnfocused(true);
+        break;
+      case "subject":
+        setSubjectUnfocused(true);
+        break;
+      case "message":
+        setMessageUnfocused(true);
+        break;
+      default:
+        throw new Error(`Unknown input name: ${inputName}`);
+    }
+  }
+
+  useEffect(() => {
+    validateEmail();
+  }, [email]);
+
   function onSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
-    alert("This form is still in construction!");
+    if (!validateForm()) {
+      setNameUnfocused(true);
+      setEmailUnfocused(true);
+      setSubjectUnfocused(true);
+      setMessageUnfocused(true);
+      return;
+    }
+
+    // TODO: Send email
+    alert("In construction!");
   }
 
   return (
@@ -17,23 +71,48 @@ export default function ContactForm() {
     >
       <h3 className="text-2xl text-textPrimary">Send me a message</h3>
       <input
-        className="w-full rounded-sm bg-primary px-3 py-2 text-textPrimary outline-none focus:outline-accent"
+        name="name"
+        className={`${
+          !name && nameUnfocused ? "outline-red" : "focus:outline-accent"
+        } w-full rounded-sm bg-primary px-3 py-2 text-textPrimary outline-none`}
         type="text"
         placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        onBlur={onInputBlur}
       />
+
       <input
-        className="w-full rounded-sm bg-primary px-3 py-2 text-textPrimary outline-none focus:outline-accent"
+        name="email"
+        className={`${
+          !emailValid && emailUnfocused ? "outline-red" : "focus:outline-accent"
+        } w-full rounded-sm bg-primary px-3 py-2 text-textPrimary outline-none`}
         type="text"
         placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        onBlur={onInputBlur}
       />
       <input
-        className="w-full rounded-sm bg-primary px-3 py-2 text-textPrimary outline-none focus:outline-accent"
+        name="subject"
+        className={`${
+          !subject && subjectUnfocused ? "outline-red" : "focus:outline-accent"
+        } w-full rounded-sm bg-primary px-3 py-2 text-textPrimary outline-none`}
         type="text"
         placeholder="Subject"
+        value={subject}
+        onChange={(e) => setSubject(e.target.value)}
+        onBlur={onInputBlur}
       />
       <textarea
-        className="min-h-[120px] rounded-sm bg-primary px-3 py-2 text-textPrimary outline-none focus:outline-accent"
+        name="message"
+        className={`${
+          !message && messageUnfocused ? "outline-red" : "focus:outline-accent"
+        } min-h-[150px] w-full rounded-sm bg-primary px-3 py-2 text-textPrimary outline-none`}
         placeholder="Message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        onBlur={onInputBlur}
       ></textarea>
 
       <AnimatedButton className={"mt-3 w-fit"} text={"Send email"} />

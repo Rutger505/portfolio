1a1bb367cca2eb287f0503f42f38a420d36c03ad
Rutger <rutger.pronk11@gmail.com>
2024-01-03 01:13:47 +0100

    First step of multilingual

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  i18n: {
+    locales: ['en', 'nl'],
+    defaultLocale: 'en',
+    // domains: [ TODO test this for production
+    //   {
+    //     domain: 'rutgerpronk.com',
+    //     defaultLocale: 'en-US',
+    //   },
+    //   {
+    //     domain: 'rutgerpronk.nl',
+    //     defaultLocale: 'nl-NL',
+    //   }
+    // ],
+  }
+}
